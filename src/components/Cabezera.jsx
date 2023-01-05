a79c4df23ca816903fb8d20321805fee08eecfdb
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-01-05 11:02:40 -0300

    componentss

---
+++ src/components/Cabezera.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Cabezera = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <div className="play"></div>
+      </div>
+
+      <h1>MisPelis</h1>
+    </header>
+  );
+};
+
+export default Cabezera;
