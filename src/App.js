165dd78f966f8ec87934e338ce25e58fe016a550
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-01-04 23:56:24 -0300

    firtss

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hola</h1>
-      <h2>Como estas</h2>
-      <p>Creo q esto hay q borrar</p>
+     
     </div>
   );
 }
