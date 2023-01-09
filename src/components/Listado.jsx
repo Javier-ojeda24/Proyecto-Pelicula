a80b842335cacc3bbbaa871f05a74a4499ef6a82
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-01-09 16:01:25 -0300

    listados

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -4,14 +4,29 @@ const Listado = ({ listado, setListado }) => {
   // const [listado, setListado] = useState([]);
 
   useEffect(() => {
-    console.log("Componente del listado de peliculas cargados");
+    // console.log("Componente del listado de peliculas cargados");
     conseguirPeli();
   }, []);
 
   const conseguirPeli = () => {
     let pelicula = JSON.parse(localStorage.getItem("pelis"));
     setListado(pelicula);
-    console.log(pelicula);
+    // console.log(pelicula);
+    return pelicula;
+  };
+  const borrarPeli = (id) => {
+    //conseguir peliculas almacenadas
+    let pelis_almacenadas = conseguirPeli();
+
+    //Filtrar las poelicula q no quiero
+    let nuevoArrayPelicula = pelis_almacenadas.filter(
+      (e) => e.id !== parseInt(id)
+    );
+    //Actualizar estado del listado
+    setListado(nuevoArrayPelicula);
+
+    //Actualizar los datos en el localStorage
+    localStorage.setItem("pelis", JSON.stringify(nuevoArrayPelicula));
   };
 
   return (
@@ -24,7 +39,14 @@ const Listado = ({ listado, setListado }) => {
               <p className="description">{e.descripcion}</p>
 
               <button className="edit">Editar</button>
-              <button className="delete">Borrar</button>
+              <button
+                className="delete"
+                onClick={() => {
+                  borrarPeli(e.id);
+                }}
+              >
+                Borrar
+              </button>
             </article>
           );
         })
