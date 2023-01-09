beb7c54b2424db6a3b277cea3fbe7b9cb19eef16
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-01-09 14:44:37 -0300

    buscar

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,38 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Listado = () => {
+  const [listado, setListado] = useState([]);
+
+  useEffect(() => {
+    console.log("Componente del listado de peliculas cargados");
+    conseguirPeli();
+  }, []);
+
+  const conseguirPeli = () => {
+    let pelicula = JSON.parse(localStorage.getItem("pelis"));
+    setListado(pelicula);
+    console.log(pelicula);
+  };
+
   return (
     <>
-      <article className="peli-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">victorroblesweb.es</p>
+      {listado === null ? (
+        <h2>No hay pelicula para mostrar</h2>
+      ) : (
+        listado.map((e) => {
+          return (
+            <article key={e.id} className="peli-item">
+              <h3 className="title">{e.titulo}</h3>
+              <p className="description">{e.descripcion}</p>
 
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
-      <article className="peli-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">victorroblesweb.es</p>
-
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
-
-      <article className="peli-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">victorroblesweb.es</p>
-
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
-
-      <article className="peli-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">victorroblesweb.es</p>
-
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
+              <button className="edit">Editar</button>
+              <button className="delete">Borrar</button>
+            </article>
+          );
+        })
+      )}
     </>
   );
 };
